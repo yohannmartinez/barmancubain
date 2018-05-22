9d1679e1fbc265c4a3a3848f32a2de75a8f54b7f
yohann martinez <yohannmartinez34@gmail.com>
2018-05-22 17:23:31 +0200

    add background to presentation on acceuil

--- js/barmancubainhome.js
+++ js/barmancubainhome.js
@@ -10,6 +10,30 @@ jQuery(document).ready(function () {
         }
     });
 
+    $('#goToAcceuil').click(function(){
+        $(window).scrollTop(0);
+        $('.menudefilant').css({"left":"-400px"});
+    });
+
+    $('#goToPres').click(function(){
+        $(window).scrollTop($('.homecontainer2').offset().top +200);
+        $('.menudefilant').css({"left":"-400px"});
+    });
+
+    $('#goToCocktails').click(function(){
+        $(window).scrollTop($('.homecontainer3').offset().top +200);
+        $('.menudefilant').css({"left":"-400px"});
+    });
+
+    $('#goToPhotos').click(function(){
+        $(window).scrollTop($('.homecontainer4').offset().top );
+        $('.menudefilant').css({"left":"-400px"});
+    });
+
+    $('#goToContacts').click(function(){
+        
+    });
+
     /* debut de menudefilant */
     
 
