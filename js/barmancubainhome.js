0880200b3f21eed57b0127a1ca5cf5136d6c077a
yohann martinez <yohannmartinez34@gmail.com>
2018-06-17 21:53:56 +0200

    modify some object interactions

--- js/barmancubainhome.js
+++ js/barmancubainhome.js
@@ -3,39 +3,39 @@ jQuery(document).ready(function () {
     /* debut de menudefilant */
     $('.boutonmenu').click(function () {
         if ($(".menudefilant").css("left") === '-400px') {
-            $('.menudefilant').css({"left":"0px"});
+            $('.menudefilant').css({ "left": "0px" });
         } else {
-            $('.menudefilant').css({"left":"-400px"});
+            $('.menudefilant').css({ "left": "-400px" });
         }
     });
 
-    $('#goToAcceuil').click(function(){
+    $('#goToAcceuil').click(function () {
         $(window).scrollTop(0);
-        $('.menudefilant').css({"left":"-400px"});
+        $('.menudefilant').css({ "left": "-400px" });
     });
 
-    $('#goToPres').click(function(){
-        $(window).scrollTop($('.homecontainer2').offset().top +200);
-        $('.menudefilant').css({"left":"-400px"});
+    $('#goToPres').click(function () {
+        $(window).scrollTop($('.partprivate').offset().top);
+        $('.menudefilant').css({ "left": "-400px" });
     });
 
-    $('#goToCocktails').click(function(){
-        $(window).scrollTop($('.homecontainer3').offset().top +200);
-        $('.menudefilant').css({"left":"-400px"});
+    $('#goToCocktails').click(function () {
+        $(window).scrollTop($('.colonnehome3').offset().top);
+        $('.menudefilant').css({ "left": "-400px" });
     });
 
-    $('#goToPhotos').click(function(){
-        $(window).scrollTop($('.homecontainer4').offset().top );
-        $('.menudefilant').css({"left":"-400px"});
+    $('#goToPhotos').click(function () {
+        $(window).scrollTop($('.homecontainer4').offset().top);
+        $('.menudefilant').css({ "left": "-400px" });
     });
 
-    $('#goToContacts').click(function(){
-        $(window).scrollTop($('.homecontainer5').offset().top );
-        $('.menudefilant').css({"left":"-400px"});
+    $('#goToContacts').click(function () {
+        $(window).scrollTop($('.homecontainer5').offset().top);
+        $('.menudefilant').css({ "left": "-400px" });
     });
 
     /* debut de menudefilant */
-    
+
 
     /* debut de homecontainer */
 
@@ -44,17 +44,30 @@ jQuery(document).ready(function () {
         $('.homecontainer').height($(window).height() + "px");
     });
 
-    $('.boutongodown').click(function(){
-        $(window).scrollTop($('.homecontainer2').offset().top +200);
+    $('.boutongodown').click(function () {
+        $(window).scrollTop($('.partprivate').offset().top);
     })
 
     /* fin de homecontainer */
 
+    /* debut de homecontainer2 */
+
+    $('.partprivate').click(function () {
+        $('.homeconatiner2extendprivate').height("400px");
+        $('.homeconatiner2extendcompany').height("0px");
+        $(window).scrollTop($('.homeconatiner2extendprivate').offset().top);
+    });
+    $('.partcompany').click(function () {
+        $('.homeconatiner2extendcompany').height("300px");
+        $('.homeconatiner2extendprivate').height("0px");
+        $(window).scrollTop($('.homeconatiner2extendcompany').offset().top);
+    });
+
     /* debut de contact */
 
-    $("#instagram").click(function() { window.location="https://www.instagram.com/barmancubain/"; });
-    $("#facebook").click(function() { window.location="https://www.facebook.com/Barman-Cubain-1543829105704825/"; });
-    $("#linkedin").click(function() { window.location="https://www.linkedin.com/in/renelier-martinez-1bb5a4127/"; });
+    $("#instagram").click(function () { window.location = "https://www.instagram.com/barmancubain/"; });
+    $("#facebook").click(function () { window.location = "https://www.facebook.com/Barman-Cubain-1543829105704825/"; });
+    $("#linkedin").click(function () { window.location = "https://www.linkedin.com/in/renelier-martinez-1bb5a4127/"; });
 
     /* fin de contact */
 
